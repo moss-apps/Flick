17c10053358b7f4300b8f811544420847fa19067
ultraelectronica <fyketonel@gmail.com>
2026-06-01 16:54:35 +0800

    Format duration as h:mm:ss when over an hour

--- lib/features/player/screens/full_player_screen.dart
+++ lib/features/player/screens/full_player_screen.dart
@@ -315,11 +315,15 @@ class _FullPlayerScreenState extends ConsumerState<FullPlayerScreen>
     _cachedTopBarTextWidth = textPainter.width;
   }
 
-  // For nice time formatting (mm:ss)
+  // For nice time formatting (h:mm:ss or mm:ss)
   String _formatDuration(Duration duration) {
     String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = duration.inHours;
     final minutes = twoDigits(duration.inMinutes.remainder(60));
     final seconds = twoDigits(duration.inSeconds.remainder(60));
+    if (hours > 0) {
+      return '$hours:$minutes:$seconds';
+    }
     return '$minutes:$seconds';
   }
 
