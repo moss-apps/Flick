323b3dee8f4ad95314bccf93a619688c769bf8d5
ultraelectronica <fyketonel@gmail.com>
2026-07-31 18:58:30 +0800

    Disable release minification and shrink resources
    - Bump version to 0.20.5-beta.6+24
    - Add Flutter migrator flags to gradle.properties

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -78,6 +78,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
