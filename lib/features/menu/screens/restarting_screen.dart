56d22b377c1eb7e21f6dad16c4fea14b780057f7
ultraelectronica <fyketonel@gmail.com>
2026-08-18 20:40:01 +0800

    Set hardcoded background color on restart screen

--- lib/features/menu/screens/restarting_screen.dart
+++ lib/features/menu/screens/restarting_screen.dart
@@ -79,7 +79,7 @@ class _RestartingScreenState extends State<RestartingScreen>
     return PopScope(
       canPop: false,
       child: Scaffold(
-        backgroundColor: AppColors.background,
+        backgroundColor: Color(0xFF040404),
         body: SafeArea(
           child: FadeTransition(
             opacity: _fade,
@@ -88,7 +88,7 @@ class _RestartingScreenState extends State<RestartingScreen>
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   AnimatedBuilder(
-                    animation: _sweepController,
+                    animation: _sweepController,  
                     builder: (context, child) {
                       return ShaderMask(
                         blendMode: BlendMode.srcIn,
