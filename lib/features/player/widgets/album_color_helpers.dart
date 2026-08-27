0b883840799322d95fdb8170b990b078e3e94197
ultraelectronica <fyketonel@gmail.com>
2026-08-28 00:52:17 +0800

    Add configurable lightness to albumAccent

--- lib/features/player/widgets/album_color_helpers.dart
+++ lib/features/player/widgets/album_color_helpers.dart
@@ -5,11 +5,15 @@ const Color _darkBase = Color(0xFF121212);
 Color albumSurface(Color albumColor, double blend) =>
     Color.lerp(_darkBase, albumColor, blend)!;
 
-Color albumAccent(Color albumColor, double blend) {
+Color albumAccent(
+  Color albumColor,
+  double blend, {
+  double lightness = 0.65,
+}) {
   final hsl = HSLColor.fromColor(albumColor);
   return hsl
       .withSaturation((hsl.saturation * 0.7).clamp(0.3, 0.8))
-      .withLightness(0.65)
+      .withLightness(lightness)
       .toColor()
       .withValues(alpha: (blend + 0.3).clamp(0.0, 1.0));
 }
