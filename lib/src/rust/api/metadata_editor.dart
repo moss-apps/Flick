67015dc874adc930f024deb1a5c09ff3d5bf9483
ultraelectronica <fyketonel@gmail.com>
2026-09-20 04:02:32 +0800

    Regenerate FRB bindings with new ALAC API functions
    Adds `alac_create_session_from_path`, `alac_read_pcm`, and
    `alac_seek_frame` to the Rust API, shifts all funcIds by 3, updates
    rustContentHash, and refreshes ignored-function comments in Dart API
    files.

--- lib/src/rust/api/metadata_editor.dart
+++ lib/src/rust/api/metadata_editor.dart
@@ -6,7 +6,7 @@
 import '../frb_generated.dart';
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 
-// These functions are ignored because they are not marked as `pub`: `apply_tag_fields`
+// These functions are ignored because they are not marked as `pub`: `apply_tag_fields`, `id3_text_frame`, `read_dsd_id3_tag`, `tag_read_result_from_id3`
 // These function are ignored because they are on traits that is not defined in current crate (put an empty `#[frb]` on it to unignore): `clone`, `clone`, `clone`, `fmt`, `fmt`, `fmt`
 
 Future<TagReadResult> readTags({required String path}) =>
